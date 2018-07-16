49045f199f0491ca604da4f1ea1b6c685dbe6904
crimsonwrench <garon312@gmail.com>
2018-07-16 21:47:33 +0300

    Filling services

--- src/main/java/com/mick/Service/ProjectService.java
+++ src/main/java/com/mick/Service/ProjectService.java
@@ -1,4 +1,28 @@
 package com.mick.Service;
 
+import com.mick.Entity.Project;
+import com.mick.Repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProjectService {
+
+    private final ProjectRepository projectRepository;
+
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public String printProjects(){
+        StringBuilder result = new StringBuilder();
+
+        result.append("pid\ttitle\n");
+
+        for(Project project : projectRepository.findAll()){
+            result.append(project.toString()).append("\n");
+        }
+        return result.toString();
+    }
 }
--- src/main/java/com/mick/Service/UserService.java
+++ src/main/java/com/mick/Service/UserService.java
@@ -1,4 +1,35 @@
 package com.mick.Service;
 
+import com.mick.Entity.User;
+import com.mick.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String printUsers(){
+        StringBuilder result = new StringBuilder();
+
+        result.append("uid\tname\n");
+
+        for(User user : userRepository.findAll()){
+            result.append(user.toString()).append("\n");
+        }
+        return result.toString();
+    }
+
+    public void insertUser(String userName){
+        userRepository.save(new User(userName));
+    }
+    public void deleteById(int id){
+        userRepository.deleteById(id);
+    }
 }
